00a71aaba3fb4fddfbff7f0ab41529285b7e4074
arthur.chang <arthur.chang@afusion.com>
2016-12-08 22:38:30 +0800

    First check-versions

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import * as jQuery  from 'jquery';
+import * as bootstrap from 'bootstrap-sass';
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
